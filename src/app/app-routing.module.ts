a6bf7433f74749dc1b23c7830736c333ee4832fe
vc-kathan <kathan.patel@viitor.cloud>
2022-05-06 14:35:47 +0530

    folder structure setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    // canActivate: [SignInGuard],
+    loadChildren: () => import('./route/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'user',
+    // canActivate: [SignOutGuard],
+    // component: LayoutComponent,
+    loadChildren: () => import('./route/pages/pages.module').then(m => m.PagesModule)
+  },
+  { path: '**', redirectTo: "auth" }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
