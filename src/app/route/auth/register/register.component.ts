01042e582766992135d7d1959c7e7956ef7e8be8
vc-kathan <kathan.patel@viitor.cloud>
2022-05-09 16:50:57 +0530

    register page & validator  Changes to be committed: 	new file:   src/app/core/validator/confirmed.validator.ts 	modified:   src/app/route/auth/login/login.component.scss 	modified:   src/app/route/auth/register/register.component.html 	modified:   src/app/route/auth/register/register.component.scss 	modified:   src/app/route/auth/register/register.component.ts

--- src/app/route/auth/register/register.component.ts
+++ src/app/route/auth/register/register.component.ts
@@ -1,20 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { ConfirmedValidator } from 'src/app/core/validator/confirmed.validator';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  // data = {
-  //   name: 'kathan',
-  //   email: 'kathanpatel0000@gmail.com',
-  //   password: '123456123456',
-  //   age: 22,
-  //   phoneNumber: '7778835898',
-  // }
   hide = true;
   chide = true;
   loading = false;
@@ -27,55 +20,60 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // this.register(this.data)
     this.registerForm = this.formBuilder.group({
       name: ['kathan', Validators.required],
-      email: ['kathan@gmail.com', [Validators.required, Validators.email]],
+      email: ['kathanpatel5555@gmail.com', [Validators.required, Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,9}$/)]],
       age: ['23', Validators.required],
       file: [''],
       fileSource: [''],
       phoneNumber: ['7778835898', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
       password: ['123456789', [Validators.required, Validators.minLength(8)]],
-      ConfirmPassword: ['1234567897', [Validators.required]]
-    });
+      ConfirmPassword: ['123456789', [Validators.required]]
+    }, {
+      validator: ConfirmedValidator('password', 'ConfirmPassword')
+    }
+    );
   }
-  // register(data: any) {
-
-  //   this._auth.RegisterUser(data).subscribe({
-  //     next: (res) => {
-  //       console.log(res);
-  //     }, error: (err) => {
-  //       console.log(err);
-  //     }
-  //   })
-  // }
 
   Registeruser() {
-    // this.loading = true
-    const formData = new FormData();
-    formData.append('file', this.registerForm.get('fileSource')?.value);
-    formData.append('name', this.registerForm.get('name')?.value);
-    formData.append('email', this.registerForm.get('email')?.value);
-    formData.append('age', this.registerForm.get('age')?.value);
-    formData.append('phoneNumber', this.registerForm.get('phoneNumber')?.value);
-    formData.append('password', this.registerForm.get('password')?.value)
+    if (this.registerForm.valid) {
+      this.loading = true
+      const formData = new FormData();
+      formData.append('profilePic', this.registerForm.get('fileSource')?.value);
+      formData.append('name', this.registerForm.get('name')?.value);
+      formData.append('email', this.registerForm.get('email')?.value);
+      formData.append('age', this.registerForm.get('age')?.value);
+      formData.append('phoneNumber', this.registerForm.get('phoneNumber')?.value);
+      formData.append('password', this.registerForm.get('password')?.value)
 
-    console.log(this.registerForm.value);
-    console.log(formData);
+      // console.log(this.registerForm.value);
 
-    formData.forEach((val, key) => {
-      console.log(key, "=>", val);
-    })
-    // this._auth.RegisterUser(formData).subscribe({
-    //   next: (res) => {
-    //     console.log(res);
-    // this.loading = false;
-    //   }, error: (err) => {
-    //     console.log(err);
-    // this.loading = false;
-    //   }
-    // })
+      // formData.forEach((val, key) => {
+      //   console.log(key, "=>", val);
+      // })
+      this._auth.RegisterUser(formData).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.loading = false;
+        }, error: (err) => {
+          console.log(err);
+          this.loading = false;
+        }
+      })
+    }
+  }
 
+  NumberValid(event: any) {
+    // console.log(event);
+    if ((event.key == "ArrowRight") || (event.key == "ArrowLeft") || (event.key == "Backspace") || (event.key == "Delete") || (event.key == "Tab") || (event.key == "Enter")) {
+
+    }
+    else if (+event.key || (+event.key == 0)) {
+      // console.log("isnumber");
+    } else {
+      // console.log("notnumber");
+      event.preventDefault();
+    }
 
   }
 
@@ -106,4 +104,8 @@ export class RegisterComponent implements OnInit {
   get ConfirmPassword() {
     return this.registerForm.get('ConfirmPassword');
   }
+  get f() {
+    return this.registerForm.controls;
+  }
+
 }
